982a4a79db1773ab7f4ece0ef999b23581144a6b
Ben Chopson <bchopson@users.noreply.github.com>
2019-06-30 17:17:56 -0400

    Initial commit
    Install next, react, express, sequelize

---
+++ pages/index.js
@@ -0,0 +1,34 @@
+function Home() {
+  return (
+    <>
+      <div>Welcome</div>
+      <p>Hello. this is some text</p>
+      <table style={{ border: '1px solid black' }}>
+        <thead>
+          <tr>
+            <th>1</th>
+            <th>2</th>
+            <th>3</th>
+            <th>4</th>
+            <th>5</th>
+            <th>6</th>
+            <th>7</th>
+            <th>8</th>
+            <th>9</th>
+            <th>10</th>
+            <th>11</th>
+            <th>12</th>
+            <th>13</th>
+            <th>14</th>
+            <th>15</th>
+            <th>16</th>
+            <th>17</th>
+            <th>18</th>
+          </tr>
+        </thead>
+      </table>
+    </>
+  );
+}
+
+export default Home;
